daa54f8b4904719bb6cf06ae5143782c38c1cf66
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-12-04 21:41:19 +0530

    pass

---
+++ pass.py
@@ -0,0 +1,10 @@
+for i in range(20):
+    if i % 10 ==0:
+        print ('it is divisible by 10')
+        pass
+    elif i % 5 == 0:
+        print ('it is divisible by 5')
+    elif i % 2 == 0:
+        print ('it is divisible by 2')
+    else:
+        print (i)
