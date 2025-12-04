78532f1dc077b90871d30cf904c44d7864bab011
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-12-04 21:33:25 +0530

    break

---
+++ break.py
@@ -0,0 +1,7 @@
+a = input("Enter a Word:")
+for b in a:
+    if (b == "o"):
+        print ("O is found in the given text.")
+        break
+    else:
+        print ("O is not found in the given text.")
